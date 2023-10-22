99bd9848bbd548ffb6908dff269b2296a89a9e03
abdulr7mann <51442494+abdulr7mann@users.noreply.github.com>
2023-10-23 00:55:15 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import os
+from codeReview.gitCode import analyze_repository, output_messages
+
+def main():
+    st.title('Code Review App')
+
+    # Create a text input for the GitHub URL or Local Path
+    url = st.text_input('Enter the GitHub URL or Local path:')
+
+    # Create a file uploader for local files
+    uploaded_file = st.file_uploader('Or upload a file:', type=['zip', 'py', 'js', 'html', 'css'])
+
+    # Create a button to trigger the analysis
+    if st.button('Analyze'):
+        if url:
+            # If a URL is provided, analyze the GitHub repository
+            st.write('Analyzing...')
+            analyze_repository(url)  # Call your analysis function
+            for message in output_messages:
+                st.write(message)
+            output_messages.clear()
+            st.write('Analysis complete.')  # You might want to show the results here
+            
+            # Display markdown reports
+            report_path = f"report/{url.split('/')[-1].replace('.git', '')}" if 'http' in url else f"report/{os.path.basename(url)}"
+            if os.path.exists(report_path):
+                reports = [f for f in os.listdir(report_path) if f.endswith('.md')]
+                selected_report = st.selectbox('Select a report:', ['Select...'] + reports)
+                if selected_report != 'Select...':
+                    with open(os.path.join(report_path, selected_report), 'r') as file:
+                        st.markdown(file.read())
+        elif uploaded_file:
+            # If a file is uploaded, analyze the local file
+            st.write('Analyzing the uploaded file...')
+            # You'll need to modify or create a new function to handle file objects or file paths
+            # analyze_file(uploaded_file)
+            st.write('Analysis complete.')  # You might want to show the results here
+        else:
+            st.warning('Please enter a GitHub URL or upload a file.')
+
+if __name__ == '__main__':
+    main()
