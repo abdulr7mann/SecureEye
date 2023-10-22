924040020978afbb423bc9a71bb7b3d6d1f0a241
abdulr7mann <51442494+abdulr7mann@users.noreply.github.com>
2023-10-22 23:46:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-from codeReview import *
+import argparse
+import sys
+from codeReview import gitCode
 
-codeReview.gitCode.analyze_file_contents(file_contents, file_name)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Analyze code from a GitHub repository or a local path.")
+    parser.add_argument('--url', type=str, help="The URL of the GitHub repository to analyze.")
+    parser.add_argument('--path', type=str, help="The full path to the local directory to analyze.")
+    args = parser.parse_args()
+
+    try:
+        if args.url:
+            gitCode.analyze_repository(args.url)
+        elif args.path:
+            gitCode.analyze_local_path(args.path)
+        else:
+            print("Please provide either a GitHub URL with --url or a local path with --path.")
+    except KeyboardInterrupt:
+        print("\nApplication interrupted by user. Exiting...")
+        sys.exit()
